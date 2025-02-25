adeb4885c35c9bb2fe95491356915bf874ff14f9
khiem13052003 <dinhkhiemk3@gmail.com>
2025-02-25 14:58:26 +0700

    fix camera

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-from PyQt6.QtWidgets import QApplication
-from views.main_window import MainWindow
-import sys
+from module import deployModel
 
-app = QApplication(sys.argv)
-window = MainWindow()
-window.show()
-sys.exit(app.exec())
+if __name__ == "__main__":
+    folder_path = r"D:\DaiHoc\Intern\ThienPhuocCompany\data_fishNet\58cm_moi"  # Thay đổi đường dẫn phù hợp
+    processor = deployModel.ImageProcessor()
+    # processor.processImgFolder(folder_path)
+    processor.realTime()  
